95df5d9d670b3173b59eea55f0c78ebd234ffb6c
klepa <pedro.klepa@gmail.com>
2020-06-24 16:48:17 -0300

    Add localStorage integration

--- scripts/main.js
+++ scripts/main.js
@@ -11,7 +11,12 @@ const inputIsRead = document.querySelector('#book-read');
 let idEnum = 0;
 let bookLibrary = [];
 
+
 // ..:: Event Listeners
+// - Local Storage management
+window.addEventListener('load', fetchLibrary);
+window.addEventListener('unload', saveLibrary);
+
 // - Modal Handling
 document.querySelector('#btn-add-form').addEventListener('click', () => modal.classList.remove('hide'));
 document.querySelector('#modal #btn-close').addEventListener('click', () => modal.classList.add('hide'));
@@ -44,7 +49,6 @@ function addBook(){
 }
 
 function createBook(bookObj) {
-    console.log(bookObj)
     let bookHTML = `
         <div class="card" data-bookId="${bookObj.id}">
             <h2>${bookObj.title}</h2>
@@ -85,10 +89,39 @@ function changeReadStatus(e) {
     bookLibrary[bookIndexInLibrary].isRead = e.target.checked;
 }
 
+
 // - Helper Functions
+function populateStorage() {
+    let bookOne = ['Budapeste', 'Chico Buarque de Holanda', 174, true];
+    let bookTwo = ['Will my cat eat my eyeballs?', 'Caitlin Doughty', 222, true];
+    let bookThree = ['The Time Machine', 'H.G. Wells', 118, true];
+    localStorage.setItem('book1', JSON.stringify(bookOne));
+    localStorage.setItem('book2', JSON.stringify(bookTwo));
+    localStorage.setItem('book3', JSON.stringify(bookThree));
+}
+
+
+function fetchLibrary() {
+    // If it is the first time entering the site, creates a storage with predetermined books
+    if (!localStorage.getItem('book1')) populateStorage()
+
+    for (let i = 1; i <= localStorage.length; i++) {
+        let bookArr = JSON.parse(localStorage.getItem(`book${i}`));
+        let book = new Book(bookArr[0], bookArr[1], bookArr[2], bookArr[3]);
+        createBook(book);
+    }
+}
+
+function saveLibrary() {
+    localStorage.clear();
+
+    let i = 1;
+    bookLibrary.forEach((book) => {
+        localStorage.setItem(`book${i}`, JSON.stringify(Object.values(book)));
+        i++;
+    });
+}
+
 
 
 // ..:: Script
-createBook(new Book('Budapeste', 'Chico Buarque de Holanda', 174, true));
-createBook(new Book('Will my cat eat my eyeballs?', 'Caitlin Doughty', 222, true));
-createBook(new Book('The Time Machine', 'H.G. Wells', 118, true));
